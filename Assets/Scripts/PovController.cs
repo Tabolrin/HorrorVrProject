29b94586e08d3700d7ffd68e536b3832cd989ab3
Tabolrin <93069446+Tabolrin@users.noreply.github.com>
2026-01-17 23:51:24 +0200

    povControllerScript

---
+++ Assets/Scripts/PovController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PovController : MonoBehaviour
+{
+    [SerializeField] Animator upAnimator;
+    [SerializeField] Animator downAnimator;
+
+    [SerializeField] Camera playerCamera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveToPlayerPOV()
+    {
+        
+    }
+    
+    public void MoveToGodCam()
+    {
+        
+    }
+
+}
